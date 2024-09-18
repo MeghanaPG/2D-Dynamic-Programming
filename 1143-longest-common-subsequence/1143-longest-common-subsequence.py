d5bba77f84344e4369e0c1ea4e11b6d2d36a6e58
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-18 08:46:35 -0400

    Time: 482 ms (60.32%), Space: 42.6 MB (26.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Time Complexity: O(m*n) (length of strings mutliplied by each other)
-        # 2D grid with 0's in it 
-        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+        # Bottom Up
+        # Time Complexity: O(n * m)
+        dp = [[ 0 for j in range(len(text2) +1)] for i in range(len(text1) + 1)]
 
-        # we will traverse from bottom right to top left 
-        for i in range(len(text1)-1, -1, -1):
-          for j in range(len(text2)-1, -1, -1):
-            if text1[i] == text2[j]:
-              # 1 + diagonal
-              dp[i][j] = 1 + dp[i+1][j+1]
-            else:
-              # max(right value, bottom value)
-              dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        # traversing the 2D grid in reverse order
+        for i in range(len(text1) -1, -1, -1):
+            for j in range(len(text2) -1, -1, -1):
+                # we take from diagonal if there is a match
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    # max of (right, below)
+                    # we don't add 1 here as the characters don't match 
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        # res will be at top left
         return dp[0][0]
